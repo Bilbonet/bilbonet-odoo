f17d86f8cd76a3d64a30b500e98d56e282b387ee
bilbonet <jesus@bilbonet.net>
2024-12-06 19:45:37 +0100

    [IMP] better heritance

--- partner_delivery_zone_extended/models/stock_picking_batch.py
+++ partner_delivery_zone_extended/models/stock_picking_batch.py
@@ -16,24 +16,25 @@ class StockPickingBatch(models.Model):
         states={"draft": [("readonly", False)]},
     )
 
-    #!This method overrides the original to include the delivery_zone_id in domain
     @api.depends('company_id', 'picking_type_id', 'state', 'delivery_zone_id')
     def _compute_allowed_picking_ids(self):
         allowed_picking_states = ['waiting', 'confirmed', 'assigned']
 
         for batch in self:
-            domain_states = list(allowed_picking_states)
-            if batch.state == 'draft':
-                domain_states.append('draft')
-            domain = [
-                ('company_id', '=', batch.company_id.id),
-                ('state', 'in', domain_states),
-            ]
-            if not batch.is_wave:
-                domain = AND([domain, [('immediate_transfer', '=', False)]])
-            if batch.picking_type_id:
-                domain += [('picking_type_id', '=', batch.picking_type_id.id)]
-            if batch.delivery_zone_id:
-                domain += [('delivery_zone_id', '=', batch.delivery_zone_id.id)]
-            batch.allowed_picking_ids = self.env['stock.picking'].search(domain)
+            if not batch.delivery_zone_id:
+                return super()._compute_allowed_picking_ids()
+            else:
+                domain_states = list(allowed_picking_states)
+                if batch.state == 'draft':
+                    domain_states.append('draft')
+                domain = [
+                    ('company_id', '=', batch.company_id.id),
+                    ('state', 'in', domain_states),
+                ]
+                if not batch.is_wave:
+                    domain = AND([domain, [('immediate_transfer', '=', False)]])
+                if batch.picking_type_id:
+                    domain += [('picking_type_id', '=', batch.picking_type_id.id)]
 
+                domain += [('delivery_zone_id', '=', batch.delivery_zone_id.id)]
+                batch.allowed_picking_ids = self.env['stock.picking'].search(domain)
