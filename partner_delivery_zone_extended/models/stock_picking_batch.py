31a9df26b339599359daece6572909ff89af88fb
bilbonet <jesus@bilbonet.net>
2024-12-27 11:22:19 +0100

    minimal changes

--- partner_delivery_zone_extended/models/stock_picking_batch.py
+++ partner_delivery_zone_extended/models/stock_picking_batch.py
@@ -1,6 +1,6 @@
 # Copyright 2024 - Bilbonet <jesus@bilbonet.net>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import _, api,fields, models
+from odoo import _, api, fields, models
 from odoo.osv.expression import AND
 
 
