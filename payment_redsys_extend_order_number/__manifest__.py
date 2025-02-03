f693eca7b0d4203312cdaf44dcf41c44e5b55b9b
bilbonet <jesus@bilbonet.net>
2025-02-03 17:29:56 +0100

    [NEW] module payment_redsys_extend_order_number

---
+++ payment_redsys_extend_order_number/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Jesus Ramiro <jesus@bilbonet.net>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Redsys Extend Order Number",
+    "summary": "Extend Redsys modulo to extend order number to 20 digits",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Payment Acquirer",
+    "website": "https://github.com/bilbonet/bilbonet-odoo/tree/14.0/payment_redsys_extend_order_number",
+    "author": "Jesus Ramiro, Bilbonet",
+    "maintainers": ["bilbopnet"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "payment_redsys",
+    ],
+    "data": [],
+}
---
+++ payment_redsys_extend_order_number/readme/CONTEXT.md
@@ -0,0 +1,1 @@
+El módulo https://github.com/OCA/l10n-spain/tree/14.0/payment_redsys por defecto trunca el numero de pedido **(Ds_Order)** a 12 caracteres. Redsys permite exender este número a 20 caracteres.
