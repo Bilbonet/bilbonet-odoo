1e3e4af960b0ede246837e0430b2b2bc52a3c5f7
bilbonet <jesus@bilbonet.net>
2025-02-15 11:42:22 +0100

    [IMP] modulos de correcciones y mejoras para ticketbai

---
+++ l10n_es_ticketbai_api_bbn/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Jesus Ramiro <jesus@bilbonet.net>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "TicketBAI - API BBN",
+    "summary": "Corrección de errores y mejoras en el módulo de TicketBAI API",
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/bilbonet/bilbonet-odoo/tree/14.0/l10n_es_ticketbai_pos_plus",
+    "author": "Jesus Ramiro, Bilbonet",
+    "maintainers": ["bilbonet"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_es_ticketbai_api",
+    ],
+    "data": [],
+}
