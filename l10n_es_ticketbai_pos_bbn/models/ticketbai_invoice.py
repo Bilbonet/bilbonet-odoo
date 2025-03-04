d503fc06858d6d1d810a2344a9517cdb1b41bf47
bilbonet <jesus@bilbonet.net>
2025-03-04 08:56:47 +0100

    [ADD] Detect and renumber duplicated POS order invoice

--- l10n_es_ticketbai_pos_bbn/models/ticketbai_invoice.py
+++ l10n_es_ticketbai_pos_bbn/models/ticketbai_invoice.py
@@ -2,20 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
 
-from odoo import api, models
+from odoo import api, models, fields
 
 from odoo.addons.l10n_es_ticketbai_api.models.ticketbai_invoice import (
-    RefundCode,
-    RefundType,
-    SiNoType,
     TicketBaiInvoiceState,
 )
 from odoo.addons.l10n_es_ticketbai_api.ticketbai.xml_schema import TicketBaiSchema
-from odoo.addons.l10n_es_ticketbai_api.models.ticketbai_response import (
-    TicketBaiCancellationResponseCode as CancellationResponseCode,
-    TicketBaiInvoiceResponseCode as InvoiceResponseCode,
-    TicketBaiResponseState as ResponseState,
-)
+
 
 _logger = logging.getLogger(__name__)
 TBAI_REJECTED_MAX_RETRIES = 5
@@ -24,6 +17,31 @@ TBAI_REJECTED_MAX_RETRIES = 5
 class TicketBAIInvoice(models.Model):
     _inherit = "tbai.invoice"
 
+    is_duplicated = fields.Boolean(
+        string="Duplicated",
+        compute="_compute_is_duplicated",
+        help="This field is used to mark the POS invoice name as duplicated",
+    )
+    
+    @api.depends("name", "state")
+    def _compute_is_duplicated(self):
+        for record in self:
+            record.is_duplicated = False
+            if record.pos_order_id and record.state == "error":
+                count = self.env['pos.order'].search_count([('l10n_es_unique_id', '=', record.name)])
+                if count > 1:
+                    record.is_duplicated = True
+
+    def renumber_pos_invoice(self):
+        """
+        Renumber the POS invoice if it is duplicated.
+        """
+        self.ensure_one()
+        if self.is_duplicated:
+            pos_order = self.name + "R"
+            self.pos_order_id.write({"l10n_es_unique_id": pos_order})
+            self.name = pos_order
+            
     def cancel_and_recreate(self):
         """
         Cancel and recreates invoices that are in an error state and have a POS order ID.
