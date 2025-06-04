cf5769573e9b15d18028fed8e5dcab8623dbfe07
bilbonet <jesus@bilbonet.net>
2025-06-04 17:36:52 +0200

    [IMP] pre-commit and fix some pre-commit warnings

--- multi_pricelist_based_items/__manifest__.py
+++ multi_pricelist_based_items/__manifest__.py
@@ -2,19 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Multi Pricelists Based Items',
-    'version': '12.0.1.0.0',
-    'category': 'Sales',
-    'license': 'AGPL-3',
-    'author': 'Jesus Ramiro (Bilbonet.NET)',
-    'website': 'https://www.bilbonet.net',
-    'depends': [
-        'product',
+    "name": "Multi Pricelists Based Items",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "author": "Jesus Ramiro (Bilbonet.NET)",
+    "website": "https://github.com/Bilbonet/bilbonet-odoo",
+    "depends": [
+        "product",
     ],
-    'data': [
-        'views/product_pricelist_views.xml',
+    "data": [
+        "views/product_pricelist_views.xml",
     ],
-    'installable': True,
-    'auto_install': False,
-    'application': False,
+    "installable": True,
+    "auto_install": False,
+    "application": False,
 }
