5336ae22ad7e07738338699996c02ee0ddc03a1f
bilbonet <jesus@bilbonet.net>
2025-10-10 12:31:51 +0200

    [MIG] multi_pricelist_based_items: Migration to 16.0

--- multi_pricelist_based_items/__manifest__.py
+++ multi_pricelist_based_items/__manifest__.py
@@ -1,20 +1,21 @@
 # Copyright 2021 Jesus Ramiro <jesus@bilbonet.net>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
     "name": "Multi Pricelists Based Items",
-    "version": "12.0.1.0.0",
+    "summary": "Calculate prices based on other price lists in a specific sequence",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
     "category": "Sales",
-    "license": "AGPL-3",
-    "author": "Jesus Ramiro (Bilbonet.NET)",
     "website": "https://github.com/Bilbonet/bilbonet-odoo",
+    "author": "Jesus Ramiro (Bilbonet)",
+    "maintainers": ["bilbonet"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
     "depends": [
         "product",
     ],
     "data": [
         "views/product_pricelist_views.xml",
     ],
-    "installable": True,
-    "auto_install": False,
-    "application": False,
 }
